108716d4862a23a08c64278eecdcdcefb7da1975
Misgan Moges <misganmoges03@gmail.com>
2022-08-11 23:59:35 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt("please enter the first value: "))
+let b = parseInt(prompt("please enter the second value: "))
+
+a = a + b;
+b = a - b;
+a = a-b;
+
+console.log(`The value of the a after swapping: ${a}`)
+console.log(`The value of the b after swapping: ${b}`);
+
